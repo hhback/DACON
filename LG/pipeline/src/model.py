34535a79af535dfd4bb5097cd141bee1197d92f9
hhback <i2019010491@korea.ac.kr>
2022-08-28 11:20:59 +0900

    [feature] add save funcition

--- LG/pipeline/src/model.py
+++ LG/pipeline/src/model.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 import time
+import pickle
+import os
 
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 class STACKING:
-
+    
     def __init__(
         self, X_train, X_test, y_train, y_test, model_list, best_n, seed, _type
     ):
@@ -28,30 +30,42 @@ class STACKING:
         self.final_select_n = None
 
     def get_stacking_ml_datasets(
-        self, model, X_train_n, y_train_n, X_test_n, n_folds, fitting=True
-    ):
-
+                                self,
+                                model,
+                                X_train_n,
+                                y_train_n,
+                                X_test_n,
+                                n_folds,
+                                fitting=True,
+                                save=False,
+                                save_path="",
+                                model_index=""):
+        
         kf = KFold(n_splits=n_folds, shuffle=True, random_state=self.seed)
-
+        
         train_fold_pred = np.zeros((X_train_n.shape[0], y_train_n.shape[1]))
         test_pred = np.zeros((X_test_n.shape[0], y_train_n.shape[1], n_folds))
-
-        for folder_counter, (train_index, valid_index) in enumerate(
-            kf.split(X_train_n, y_train_n)
-        ):
-
+        
+        for folder_counter, (train_index, valid_index) in enumerate(kf.split(X_train_n, y_train_n)):
+        
             X_tr = X_train_n[train_index]
             y_tr = y_train_n[train_index]
             X_te = X_train_n[valid_index]
-
-            if fitting == True:
+            
+            if fitting:
                 model.fit(X_tr, y_tr)
+
+            if save:
+                model_save_path = os.path.join(save_path, "%s_%s.pkl"%(model_index, folder_counter))
+
+                with open(model_save_path, "wb") as f:
+                    pickle.dump(model, f)
 
             train_fold_pred[valid_index] = model.predict(X_te)
             test_pred[:, :, folder_counter] = model.predict(X_test_n)
-
+            
         test_pred_mean = np.mean(test_pred, axis=2)
-
+        
         return train_fold_pred, test_pred_mean
 
     def lg_nrmse(self, gt, preds):
@@ -67,7 +81,7 @@ class STACKING:
 
         return score
 
-    def run_level0(self):
+    def run_level0(self, save_path=""):
 
         meta_ml_X_train = []
         meta_ml_X_test = []
@@ -78,13 +92,24 @@ class STACKING:
 
             print("Number %s :" % (index), estimator)
             start = time.time()
-            temp_X_train, temp_X_test = self.get_stacking_ml_datasets(
-                estimator, self.X_train, self.y_train.values, self.X_test, self.n_folds
-            )
-            meta_ml_X_train.append(temp_X_train)
-            meta_ml_X_test.append(temp_X_test)
-            end = time.time()
-            print(f"{end - start:.2f} sec")
+
+            if self._type == "real":
+              temp_X_train, temp_X_test = self.get_stacking_ml_datasets(
+                  estimator, self.X_train, self.y_train.values, self.X_test, self.n_folds, True, True, save_path, index
+              )
+              meta_ml_X_train.append(temp_X_train)
+              meta_ml_X_test.append(temp_X_test)
+              end = time.time()
+              print(f"{end - start:.2f} sec")
+
+            else:
+              temp_X_train, temp_X_test = self.get_stacking_ml_datasets(
+                  estimator, self.X_train, self.y_train.values, self.X_test, self.n_folds
+              )
+              meta_ml_X_train.append(temp_X_train)
+              meta_ml_X_test.append(temp_X_test)
+              end = time.time()
+              print(f"{end - start:.2f} sec")
 
         if self._type == "real":
 
@@ -111,7 +136,7 @@ class STACKING:
         self.meta_ml_X_train = np.array(meta_ml_X_train)[score_index[: self.best_n]]
         self.meta_ml_X_test = np.array(meta_ml_X_test)[score_index[: self.best_n]]
 
-    def run_level1(self):
+    def run_level1(self, save=False, save_path=""):
 
         if self._type == "real":
 
@@ -121,6 +146,12 @@ class STACKING:
             temp_X_test = self.meta_ml_X_test.mean(axis=0)
 
             meta_clf.fit(temp_X_train, self.y_train)
+
+            if save:
+              model_save_path = os.path.join(save_path, "meta_clf.pkl")
+              with open(model_save_path, "wb") as f:
+                      pickle.dump(meta_clf, f)
+
             prediction = meta_clf.predict(temp_X_test)
 
             return prediction
